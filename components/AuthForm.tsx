b4507722c7094d46e3d1136a9937cd5d5f1db077
Jacobo <yosoyjacobo23@gmail.com>
2025-06-04 15:53:45 -0700

    Refactor AuthForm

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -41,35 +41,39 @@ const AuthForm = () => {
     <div className="card-border lg:min-w-[566px]">
         <div className="flex flex-col gap=6 card py-14 px-10">
             <div className="flex flex-row gap-2 justify-center">
-                <Image src="/logo.svg" alt="logo" height={32} width={38}/>
+                <Image src="/logo.svg" 
+                    alt="logo" 
+                    height={32} 
+                    width={38}/>
                     <h2 className="text-primary-100">Hirely</h2>
             </div>
-        </div>
-         <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-        <FormField
-          control={form.control}
-          name="username"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Username</FormLabel>
-              <FormControl>
-                <Input placeholder="shadcn" {...field} />
-              </FormControl>
-              <FormDescription>
-                This is your public display name.
-              </FormDescription>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <Button type="submit">Submit</Button>
-      </form>
-    </Form>
-  )
+            <h3>Practice job interview with AI</h3>
 
-    </div>
-  )
-}
+            <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+            <FormField
+            control={form.control}
+            name="username"
+            render={({ field }) => (
+                <FormItem>
+                <FormLabel>Username</FormLabel>
+                <FormControl>
+                    <Input placeholder="shadcn" {...field} />
+                </FormControl>
+                <FormDescription>
+                    This is your public display name.
+                </FormDescription>
+                <FormMessage />
+                </FormItem>
+            )}
+            />
+            <Button type="submit">Submit</Button>
+        </form>
+        </Form>  
+
+        </div>
+        </div>
+    )
+    }
 
 export default AuthForm
