d7e2bfe066985b330ccdb773e58a289c03153219
Jacobo <yosoyjacobo23@gmail.com>
2025-06-09 12:15:26 -0700

    Enhance AuthForm with form validation and user feedback; add FormField component for reusable form fields

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -1,41 +1,59 @@
 "use client"
 import Image from "next/image"
+import Link from "next/link"
 
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
 import { z } from "zod"
 
+import { toast } from "sonner";
 import { Button } from "@/components/ui/button"
 import {
   Form,
-  FormControl,
-  FormDescription,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
 } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
+import FormField from "./FormField"
+import { useRouter } from "next/navigation"
  
+const authFormSchema = (type: FormType) => {
+  return z.object({
+    name: type === 'sign-up' ? z.string().min(3) : z.string().optional(),
+    email: z.string().email(),
+    password: z.string().min(3),
+  })
+}
 
-const formSchema = z.object({
-  username: z.string().min(2).max(50),
-})
-const AuthForm = () => {
-  // 1. Define your form.
+const AuthForm = ({type}: {type: FormType}) => {
+  const router = useRouter();
+
+  const formSchema = authFormSchema(type);
+
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
-      username: "",
+      name: "",
+      email: "",
+      password: "",
     },
   })
  
-  // 2. Define a submit handler.
   function onSubmit(values: z.infer<typeof formSchema>) {
-    // Do something with the form values.
-    // ✅ This will be type-safe and validated.
+   try{
+    if (type === 'sign-up') {
+      toast.success("Account created successfully!");
+        router.push('/sign-in');
+    } else {
+       toast.success("Sign in successfully!");
+        router.push('/');
+    }
+   } catch (error) {
+    console.log(error);
+    toast.error(`There was an error ${error}`)
+   }
     console.log(values)
   }
+
+  const isSignIn = type === 'sign-in';
 
   return (
     <div className="card-border lg:min-w-[566px]">
@@ -50,28 +68,42 @@ const AuthForm = () => {
             <h3>Practice job interview with AI</h3>
 
             <Form {...form}>
-        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-            <FormField
-            control={form.control}
-            name="username"
-            render={({ field }) => (
-                <FormItem>
-                <FormLabel>Username</FormLabel>
-                <FormControl>
-                    <Input placeholder="shadcn" {...field} />
-                </FormControl>
-                <FormDescription>
-                    This is your public display name.
-                </FormDescription>
-                <FormMessage />
-                </FormItem>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="w-full space-y-6 mt-4 form">
+            {!isSignIn && (
+              <FormField 
+              control={form.control} 
+              name="name" 
+              label="Name" 
+              placeholder="Your name" />
             )}
-            />
-            <Button type="submit">Submit</Button>
-        </form>
-        </Form>  
+            
+            <FormField 
+              control={form.control} 
+              name="email" 
+              label="Email" 
+              placeholder="Your Email Address" 
+              type="email" />
 
-        </div>
+            <FormField 
+              control={form.control} 
+              name="password" 
+              label="Password" 
+              placeholder="Enter your password"
+              type="password" />
+
+
+            <Button type="submit" className="btn">{isSignIn ? 'Sign In' : 'Create An Account'}</Button>
+            
+            </form>
+        </Form>  
+        <p className="text-center">
+            {isSignIn ? "Create an account" : "Already have an account?"}
+            <Link href={isSignIn ? "/sign-up  " : "/sign-in"} className="font-bold text-user-primary ml-1">
+            {!isSignIn ? "Sign In" : "Sign Up"}
+            </Link>
+        </p> 
+
+            </div>
         </div>
     )
     }
