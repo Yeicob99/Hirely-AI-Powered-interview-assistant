d45cb90dfdfaa6133fc4ae6ac3a01fdd2d45112c
Jacobo <yosoyjacobo23@gmail.com>
2025-06-20 20:12:29 -0400

    feat(auth): implement user authentication with Firebase
    - Added Firebase and Firebase Admin SDK to the project dependencies.
    - Created auth.actions.ts to handle user sign-up, sign-in, session management, and user retrieval.
    - Implemented signUp function to create a new user and store user data in Firestore.
    - Implemented signIn function to authenticate users and set session cookies.
    - Added setSessionCookie function to manage session cookies securely.
    - Implemented getCurrentUser function to retrieve the current authenticated user.
    - Added isAuthenticated function to check if a user is logged in.

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,19 +1,25 @@
 import Link  from 'next/link';
 import React, { ReactNode } from 'react'
 import Image from 'next/image'
+import { isAuthenticated } from '@/lib/actions/auth.actions';
+import { redirect } from 'next/navigation';
 
-const Rootlayout = ({children}: {children: ReactNode}) => {
+const RootLayout = async ({children}: {children: ReactNode}) => {
+  const isUserAuthenticated = await isAuthenticated();
+
+  if (!isUserAuthenticated) redirect('/sign-in');
+
   return (
     <div className="root-layout">
       <nav>
         <Link href="/" className="flex items-center gap-2" >
           <Image src="/logo.svg" alt="Hirely Logo" width={38} height={32} />
           <h2 className= "text-primary-100"> Hirely</h2>
-          </Link>
+        </Link>
       </nav>
       {children}
     </div>
   )
 }
 
-export default Rootlayout
+export default RootLayout
